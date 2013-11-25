0f4018e45747b2c85e4d95cea4c49e335d8894c3
Mirko <mirko.zaff@gmail.com>
2013-11-25 10:20:58 +0100

    logout

---
+++ WebForum/src/java/servlet_package/ServletLogout.java
@@ -0,0 +1,85 @@
+package servlet_package;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet(name = "ServletLogout", urlPatterns = {"/ServletLogout"})
+public class ServletLogout extends HttpServlet {
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        String filename = "forumHTML/signin.html";
+        
+        HttpSession session = request.getSession();
+        session.invalidate();     
+        
+        try {
+           ServletContext context = getServletContext();
+           InputStream inp = context.getResourceAsStream(filename);
+           if (inp != null) {
+               InputStreamReader isr = new InputStreamReader(inp);
+               BufferedReader reader = new BufferedReader(isr);
+               String text = "";
+               while ((text = reader.readLine()) != null) {
+                   out.println(text);
+               }
+           }
+        }finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
