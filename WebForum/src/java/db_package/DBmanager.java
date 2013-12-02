643550d8d111fdf6931050cc6fcbe68bbc28f083
Mirko <mirko.zaff@gmail.com>
2013-12-02 11:41:08 +0100

    fix errori merge =P

--- WebForum/src/java/db_package/DBmanager.java
+++ WebForum/src/java/db_package/DBmanager.java
@@ -255,9 +255,6 @@ public class DBmanager implements Serializable{
          }   
          }
      }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
      public void modificagruppo(String gname, String newgname, String gadmin, String[] utentiNuovoGruppo) throws SQLException{
          
@@ -290,12 +287,6 @@ public class DBmanager implements Serializable{
             stm2.close();
          }
      }
-=======
->>>>>>> 4a3f063926eb33c5ec96ee3f86c293322bb674ef
-=======
->>>>>>> 4a3f063926eb33c5ec96ee3f86c293322bb674ef
-=======
->>>>>>> 4a3f063926eb33c5ec96ee3f86c293322bb674ef
      
      public ArrayList<String> utentiPartecipantiPDF(String gname, String gadmin)throws SQLException{
         
